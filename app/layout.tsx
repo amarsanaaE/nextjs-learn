b78ab0a4d8d64f667cc607413bdb3831f8cf64e2
amarsanaaE <amarsanaa@ubisol.mn>
2025-04-14 11:53:06 +0800

    ghj

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import { Metadata } from "next";
-import Script from "next/script";
 import FacebookChatPackage from "@/components/reactFb";
 import "./globals.css";
 
@@ -19,33 +18,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
-        <Script
-          id="facebook-jssdk"
-          strategy="lazyOnload"
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.fbAsyncInit = function() {
-                FB.init({
-                  appId: '${
-                    process.env.NEXT_PUBLIC_FACEBOOK_APP_ID ||
-                    "1427973608364187"
-                  }',
-                  autoLogAppEvents: true,
-                  xfbml: true,
-                  version: 'v18.0'
-                });
-              };
-            `,
-          }}
-        />
-        <Script
-          strategy="lazyOnload"
-          src="https://connect.facebook.net/en_US/sdk.js"
-          crossOrigin="anonymous"
-          nonce="rAnd0m"
-        />
-      </head>
       <body className="antialiased">
         {children}
         <FacebookChatPackage />
--- components/reactFb.tsx
+++ components/reactFb.tsx
@@ -1,38 +1,97 @@
 "use client";
 import React, { useEffect } from "react";
 
+// Declare global types for window object extensions
+declare global {
+  interface Window {
+    __ow: {
+      organizationId: string;
+      integration_name: string;
+      product_name: string;
+      asyncInit?: boolean;
+    };
+    OpenWidget: any;
+  }
+}
+
 export default function FacebookChatPackage() {
-  const pageId = process.env.NEXT_PUBLIC_FACEBOOK_PAGE_ID || "597355006801976";
-
   useEffect(() => {
-    // Check if Facebook SDK is loaded
-    if (typeof window !== "undefined" && "FB" in window) {
-      window.FB.XFBML.parse();
-    }
+    // Define OpenWidget configuration
+    window.__ow = window.__ow || {};
+    window.__ow.organizationId = "4818368b-66af-4242-ac76-3b23f7af3d1b";
+    window.__ow.integration_name = "manual_settings";
+    window.__ow.product_name = "openwidget";
 
-    // Add event listener for when FB SDK is loaded asynchronously
-    const handleFbInit = () => {
-      if (typeof window !== "undefined" && "FB" in window) {
-        window.FB.XFBML.parse();
+    // Initialize OpenWidget
+    (function (n: Window, t: Document, c: Function) {
+      function i(n: any[]): any {
+        return e._h ? e._h.apply(null, n) : e._q.push(n);
       }
-    };
 
-    window.addEventListener("fbAsyncInit", handleFbInit);
+      const e: {
+        _q: any[];
+        _h: any | null;
+        _v: string;
+        on: Function;
+        once: Function;
+        off: Function;
+        get: Function;
+        call: Function;
+        init: Function;
+      } = {
+        _q: [],
+        _h: null,
+        _v: "2.0",
+        on: function () {
+          i(["on", c.call(arguments)]);
+        },
+        once: function () {
+          i(["once", c.call(arguments)]);
+        },
+        off: function () {
+          i(["off", c.call(arguments)]);
+        },
+        get: function () {
+          if (!e._h)
+            throw new Error("[OpenWidget] You can't use getters before load.");
+          return i(["get", c.call(arguments)]);
+        },
+        call: function () {
+          i(["call", c.call(arguments)]);
+        },
+        init: function () {
+          const n = t.createElement("script");
+          n.async = true;
+          n.type = "text/javascript";
+          n.src = "https://cdn.openwidget.com/openwidget.js";
+          t.head.appendChild(n);
+        },
+      };
 
-    // Cleanup
-    return () => {
-      window.removeEventListener("fbAsyncInit", handleFbInit);
-    };
-  }, []);
+      !n.__ow.asyncInit && e.init();
+      n.OpenWidget = n.OpenWidget || e;
+    })(window, document, [].slice);
+  }, []); // Empty dependency array ensures the script runs only once on mount
 
   return (
     <>
-      <div
-        className="fb-customerchat"
-        data-attribution="biz_inbox"
-        data-page_id={pageId}
-        data-theme_color="#0070F3"
-      ></div>
+      <noscript>
+        You need to{" "}
+        <a
+          href="https://www.openwidget.com/enable-javascript"
+          rel="noopener nofollow"
+        >
+          enable JavaScript
+        </a>{" "}
+        to use the communication tool powered by{" "}
+        <a
+          href="https://www.openwidget.com/"
+          rel="noopener nofollow"
+          target="_blank"
+        >
+          OpenWidget
+        </a>
+      </noscript>
     </>
   );
 }
